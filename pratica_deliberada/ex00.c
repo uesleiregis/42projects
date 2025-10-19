ea7d95a9848eddd090d7443c6e215f07f1fa6b7d
ueslei <14273677+uesleiregis@users.noreply.github.com>
2025-10-19 16:17:45 +0100

    update

---
+++ pratica_deliberada/ex00.c
@@ -0,0 +1,15 @@
+#include <fcntl>
+
+/*
+Criar e Escrever em um Arquivo
+Crie um programa que:
+
+Abre (ou cria, se não existir) o arquivo chamado "saida.txt" para escrita.
+
+Escreve a mensagem Olá, arquivo!﻿ no arquivo.
+
+Fecha o arquivo.
+
+*/
+
+open("text.txt", O_RDWR | );
